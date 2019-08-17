fe9974f0ab141476bed59b8e80273a1479276a05
Gabriel Correa <correa.gabrielsilva@outlook.com>
2019-08-17 12:07:12 -0300

    Estou salvando o projeto_solution

---
+++ prj15_revisao2/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace prj15_revisao2
+{
+    class Funcionario
+    {
+        public string Nome { get; private set; }
+        public string Cargo { get; private set; }
+        public double Desconto { get; private set; }
+        public decimal Salario { get; private set; }
+
+        public Funcionario(string nome ,string cargo, decimal salario, double desconto)
+        {
+            this.Nome = nome;
+            this.Cargo = cargo;
+            this.Salario = salario;
+            this.Desconto = desconto;
+        }
+        public decimal SaLiquido()
+        {
+            return Salario -= (Salario *(decimal) Desconto) / 100;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} você possui o cargo de {1}, com o desconto de {2}%. " +
+            "Seu salário líquido é de R$ {3}", Nome, Cargo, Desconto.ToString("f2", CultureInfo.InvariantCulture), SaLiquido());
+        }
+    }
+}
---
+++ prj15_revisao2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj15_revisao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o nome do Funcionário: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Informe o cargo do Funcionário: ");
+            string cargo = (Console.ReadLine());
+
+            Console.Write("Informe o salário do Funcionário: ");
+            decimal salario = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Informe a Porcentagem de desconto: ");
+            double desconto = double.Parse(Console.ReadLine());
+
+            Funcionario func = new Funcionario(nome, cargo, salario, desconto);
+
+            Console.WriteLine();
+            Console.WriteLine(func);
+
+            
+        }
+    }
+}
